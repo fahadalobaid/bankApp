d53327189b514b18f828618a1e3b2e9f3cb7bee3
Aziz-albelushi02 <ksa.xo1999@gmail.com>
2022-06-29 20:09:39 +0300

    push

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,1 +1,20 @@
+import 'package:bank_app/main.dart';
+import 'package:flutter/material.dart';
 
+class homePage extends StatelessWidget {
+  const homePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "home",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(title: Text("Home")),
+      ),
+    );
+  }
+}
