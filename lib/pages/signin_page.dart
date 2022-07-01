9fc95d32f8547f1e079c74e28dbab23ca86ff576
Aziz-albelushi02 <ksa.xo1999@gmail.com>
2022-07-01 07:13:47 +0300

    created a sign in UI and created a sign in router at services folder

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -1,36 +1,38 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class signin extends StatelessWidget {
-  const signin({Key? key}) : super(key: key);
-
+class SigninPage extends StatelessWidget {
+  SigninPage({Key? key}) : super(key: key);
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Container(
-      width: double.infinity,
-      height: size.height,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/background.png",
-              width: size.width * 0.35,
-            ),
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/background.png",
-              width: size.width * 0.4,
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        title: const Text("Sign in"),
       ),
-    ));
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const Text("Sign In"),
+            TextField(
+              decoration: const InputDecoration(hintText: 'Username'),
+              controller: usernameController,
+            ),
+            TextField(
+              decoration: const InputDecoration(hintText: 'Password'),
+              controller: passwordController,
+              obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Sign In"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
